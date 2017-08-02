8c9b14c02f6690a35d659799c8ee0722ad7cb8e9
nikhil <nikhil4596@gmail.com>
2017-08-02 17:22:46 -0400

    Initial Commit

---
+++ SimpleANN.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('C:\tmp\Mnist_data', one_hot=True)
+
+hl1_nodes = 500
+hl2_nodes = 500
+hl3_nodes = 500
+num_classes = 10
+num_epochs = 10
+batch_size = 100
+
+x = tf.placeholder('float',shape=[None, 784])
+y = tf.placeholder('float')
+
+def model(data):
+    pass 
