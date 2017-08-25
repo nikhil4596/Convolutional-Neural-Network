9841eac1810226ca176e0685a3054fc3e4707911
nikhil <nikhil4596@gmail.com>
2017-08-25 15:07:25 -0400

    Finished the CNN

--- ConvolutionalNeuralNetwork.py
+++ ConvolutionalNeuralNetwork.py
@@ -3,41 +3,47 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('C:\\tmp\Mnist_data', one_hot=True)
 
-hl1_nodes = 500
-hl2_nodes = 500
-hl3_nodes = 500
 num_classes = 10
 num_epochs = 10
-batch_size = 100
+batch_size = 128
 
 x = tf.placeholder('float', shape=[None, 784])
 y = tf.placeholder('float')
 
 
-def model(data):
-    # Created the Model with 3 hidden layers and one output layer
+def model(x):
+    # Created the Model with 2 Convolutional Layers and one Fully Connected layer
 
-    hidden_layer_1 = {'weights': tf.Variable(tf.random_normal([784, hl1_nodes])),
-                      'biases': tf.Variable(tf.random_normal([hl1_nodes]))}
-    hidden_layer_2 = {'weights': tf.Variable(tf.random_normal([hl1_nodes, hl2_nodes])),
-                      'biases': tf.Variable(tf.random_normal([hl2_nodes]))}
-    hidden_layer_3 = {'weights': tf.Variable(tf.random_normal([hl2_nodes, hl3_nodes])),
-                      'biases': tf.Variable(tf.random_normal([hl3_nodes]))}
-    output_layer = {'weights': tf.Variable(tf.random_normal([hl3_nodes, num_classes])),
-                    'biases': tf.Variable(tf.random_normal([num_classes]))}
+    weights = {'conv_layer_1': tf.Variable(tf.random_normal([5, 5, 1, 32])),
+               'conv_layer_2': tf.Variable(tf.random_normal([5, 5, 32, 64])),
+               'fully_connected': tf.Variable(tf.random_normal([7 * 7 * 64, 1024])),
+               'output': tf.Variable(tf.random_normal([1024, num_classes]))}
+    biases = {'conv_layer_1': tf.Variable(tf.random_normal([32])),
+              'conv_layer_2': tf.Variable(tf.random_normal([64])),
+              'fully_connected': tf.Variable(tf.random_normal([1024])),
+              'output': tf.Variable(tf.random_normal([num_classes]))}
 
-    # Feed Forward the Model
+    x = tf.reshape(x, shape=[-1,28,28,1])
 
-    l1 = tf.nn.relu(tf.matmul(data, hidden_layer_1['weights']) + hidden_layer_1['biases'])
-    l2 = tf.nn.relu(tf.matmul(l1, hidden_layer_2['weights']) + hidden_layer_2['biases'])
-    l3 = tf.nn.relu(tf.matmul(l2, hidden_layer_3['weights']) + hidden_layer_3['biases'])
-    output = (tf.matmul(l3, output_layer['weights']) + output_layer['biases'])
+    conv1 = conv2d(x, weights['conv_layer_1'])
+    conv1 = maxpool2d(conv1)
+    conv2 = conv2d(conv1, weights['conv_layer_2'])
+    conv2 = maxpool2d(conv2)
 
+    fc = tf.reshape(conv2,[-1, 7*7*64])
+    fc = tf.nn.relu(tf.matmul(fc, weights['fully_connected']) + biases['fully_connected'])
+
+    output = tf.matmul(fc, weights['output']) + biases['output']
     return output
 
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
 
-def train(data):
-    prediction = model(data)
+def maxpool2d(x):
+    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+def train(x):
+    prediction = model(x)
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
